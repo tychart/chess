4b2d391d75926ef668d0ff40811f54abd944f08c
tychart <tyler@thechartrands.com>
2024-09-12 23:38:05 -0600

    Added printlns to begining and end of program

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -4,12 +4,14 @@ public class Main {
     public static void main(String[] args) {
 //        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
 //        System.out.println("♕ 240 Chess Client: " + piece);
-
+        System.out.println("Started Program");
+        
         ChessGame game = new ChessGame();
         ChessBoard board = new ChessBoard();
 
         board.addPiece(new ChessPosition(0, 7), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
         System.out.println(board);
 
+        System.out.println("Finished Program");
     }
 }
