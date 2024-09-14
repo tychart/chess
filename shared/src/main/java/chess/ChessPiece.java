037bb816d6726b134556ff14592ab35dc0f38391
tychart <tyler@thechartrands.com>
2024-09-13 21:28:18 -0600

    Starting to add the piece moves logic

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -12,9 +12,10 @@ import java.util.HashSet;
 public class ChessPiece {
 
     private final ChessGame.TeamColor pieceColor;
-    private final ChessPiece.PieceType type;
+    private final PieceType type;
+    Collection<ChessMove> movesPossible = new HashSet<>();
 
-    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+    public ChessPiece(ChessGame.TeamColor pieceColor, PieceType type) {
 
         this.pieceColor = pieceColor;
         this.type = type;
@@ -31,6 +32,17 @@ public class ChessPiece {
         KNIGHT,
         ROOK,
         PAWN
+    }
+
+    private enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+        UPLEFT,
+        UPRIGHT,
+        DOWNLEFT,
+        DOWNRIGHT,
     }
 
     /**
@@ -62,52 +74,84 @@ public class ChessPiece {
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
 //        throw new RuntimeException("Not implemented");
-        Collection<ChessMove> return_collection = new HashSet<>();
+        this.movesPossible = new HashSet<>();
 
         switch (this.type) {
-            case KING -> {
-                return this.movesKing(return_collection, board, myPosition);
-            }
-            case QUEEN -> {
-                return this.movesQueen();
-            }
+            case KING -> this.movesKing(board, myPosition);
+            case QUEEN -> this.movesQueen(board, myPosition);
 
         }
 
+        return this.movesPossible;
     }
 
-    private Collection<ChessMove> movesKing(Collection<ChessMove> return_set, ChessBoard board, ChessPosition myPosition) {
+    private void movesKing(ChessBoard board, ChessPosition myPosition) {
 //        return Collection(new ChessMove(new ChessPosition(1,2), new ChessPosition(4, 5), this.type);
-
+        search(board, myPosition, Direction.UP, 1);
+        search(board, myPosition, Direction.DOWN, 1);
+        search(board, myPosition, Direction.LEFT, 1);
+        search(board, myPosition, Direction.RIGHT, 1);
+        search(board, myPosition, Direction.UPLEFT, 1);
+        search(board, myPosition, Direction.UPRIGHT, 1);
+        search(board, myPosition, Direction.DOWNLEFT, 1);
+        search(board, myPosition, Direction.DOWNRIGHT, 1);
 
     }
 
-    private Collection<ChessMove> movesQueen(ChessBoard board, ChessPosition myPosition) {
-        return true;
+    private void movesQueen(ChessBoard board, ChessPosition myPosition) {
+        search(board, myPosition, Direction.UP, 10);
+        search(board, myPosition, Direction.DOWN, 10);
+        search(board, myPosition, Direction.LEFT, 10);
+        search(board, myPosition, Direction.RIGHT, 10);
+        search(board, myPosition, Direction.UPLEFT, 10);
+        search(board, myPosition, Direction.UPRIGHT, 10);
+        search(board, myPosition, Direction.DOWNLEFT, 10);
+        search(board, myPosition, Direction.DOWNRIGHT, 10);
+
     }
 
-    private Collection<ChessMove> movesBishop(ChessBoard board, ChessPosition myPosition) {
-        return true;
-    }
+//    private Collection<ChessMove> movesBishop(ChessBoard board, ChessPosition myPosition) {
+//        return true;
+//    }
+//
+//    private Collection<ChessMove> movesKnight() {
+//        return true;
+//    }
+//
+//    private Collection<ChessMove> movesRook() {
+//        return true;
+//    }
+//
+//    private Collection<ChessMove> movesPawn() {
+//        return true;
+//    }
 
-    private Collection<ChessMove> movesKnight() {
-        return true;
-    }
+    private void search(ChessBoard board, ChessPosition myPosition, ChessPiece.Direction direction, int numSearch) {
 
-    private Collection<ChessMove> movesRook() {
-        return true;
-    }
+        ChessPosition newPos = null;
 
-    private Collection<ChessMove> movesPawn() {
-        return true;
-    }
+        for (int i = 0; i < numSearch; i++) {
 
-    private Collection<ChessMove> searchUp(Collection<ChessMove> return_set, ChessBoard board, ChessPosition myPosition) {
+            switch (direction) {
+                case UP -> newPos = new ChessPosition(myPosition.getRow() + i, myPosition.getColumn());
+                case DOWN -> newPos = new ChessPosition(myPosition.getRow() - i, myPosition.getColumn());
+                case LEFT -> newPos = new ChessPosition(myPosition.getRow(), myPosition.getColumn() - i);
+                case RIGHT -> newPos = new ChessPosition(myPosition.getRow(), myPosition.getColumn() + i);
+                case UPLEFT -> newPos = new ChessPosition(myPosition.getRow() + i, myPosition.getColumn() - i);
+                case UPRIGHT -> newPos = new ChessPosition(myPosition.getRow() + i, myPosition.getColumn() + i);
+                case DOWNLEFT -> newPos = new ChessPosition(myPosition.getRow() - i, myPosition.getColumn() - i);
+                case DOWNRIGHT -> newPos = new ChessPosition(myPosition.getRow() - i, myPosition.getColumn() + i);
+                default ->
+                        throw new RuntimeException("Used Wrong Direction! Direction Entered: " + direction + " Expected Enum of ChessPiece.Direction");
+            }
 
-        ChessPosition new_pos = new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn());
-        if (board.getPiece(new_pos) != null) {
-
+            if (board.getPiece(newPos) == null) {
+                this.movesPossible.add(new ChessMove(myPosition, newPos, this.type));
+            } else {
+                return;
+            }
         }
+
     }
 
     @Override
