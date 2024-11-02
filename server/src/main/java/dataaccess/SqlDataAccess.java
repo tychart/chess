0f0ba08bde7f0b7e2275fd70f69c17a07a0b85b2
tychart <tyler@thechartrands.com>
2024-11-01 18:41:41 -0600

    Edited SqlDataAccess.java to clean it up code quality wise

--- server/src/main/java/dataaccess/SqlDataAccess.java
+++ server/src/main/java/dataaccess/SqlDataAccess.java
@@ -6,21 +6,15 @@ import com.google.gson.Gson;
 //import model.Pet;
 //import model.PetType;
 
-import java.util.ArrayList;
-import java.util.Collection;
+
 import java.sql.*;
 import java.util.Map;
 import java.util.HashMap;
-
-import static dataaccess.SqlDataAccess.addPersonTest;
-import static java.sql.Statement.RETURN_GENERATED_KEYS;
-import static java.sql.Types.NULL;
 
 import com.google.protobuf.ServiceException;
 import model.AuthData;
 import model.GameData;
 import model.UserData;
-import server.ErrorResponse;
 
 
 public class SqlDataAccess implements DataAccess {
@@ -56,116 +50,6 @@ public class SqlDataAccess implements DataAccess {
     public SqlDataAccess() {
         confirmDatabaseStructureExists();
     }
-
-//    public Pet addPet(Pet pet) throws ResponseException {
-//        var statement = "INSERT INTO pet (name, type, json) VALUES (?, ?, ?)";
-//        var json = new Gson().toJson(pet);
-//        var id = executeUpdate(statement, pet.name(), pet.type(), json);
-//        return new Pet(id, pet.name(), pet.type());
-//    }
-//
-//    public Pet getPet(int id) throws ResponseException {
-//        try (var conn = DatabaseManager.getConnection()) {
-//            var statement = "SELECT id, json FROM pet WHERE id=?";
-//            try (var ps = conn.prepareStatement(statement)) {
-//                ps.setInt(1, id);
-//                try (var rs = ps.executeQuery()) {
-//                    if (rs.next()) {
-//                        return readPet(rs);
-//                    }
-//                }
-//            }
-//        } catch (Exception e) {
-//            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
-//        }
-//        return null;
-//    }
-//
-//    public Collection<Pet> listPets() throws ResponseException {
-//        var result = new ArrayList<Pet>();
-//        try (var conn = DatabaseManager.getConnection()) {
-//            var statement = "SELECT id, json FROM pet";
-//            try (var ps = conn.prepareStatement(statement)) {
-//                try (var rs = ps.executeQuery()) {
-//                    while (rs.next()) {
-//                        result.add(readPet(rs));
-//                    }
-//                }
-//            }
-//        } catch (Exception e) {
-//            throw new ResponseException(500, String.format("Unable to read data: %s", e.getMessage()));
-//        }
-//        return result;
-//    }
-//
-//    public void deletePet(Integer id) throws ResponseException {
-//        var statement = "DELETE FROM pet WHERE id=?";
-//        executeUpdate(statement, id);
-//    }
-//
-//    public void deleteAllPets() throws ResponseException {
-//        var statement = "TRUNCATE pet";
-//        executeUpdate(statement);
-//    }
-//
-//    private Pet readPet(ResultSet rs) throws SQLException {
-//        var id = rs.getInt("id");
-//        var json = rs.getString("json");
-//        var pet = new Gson().fromJson(json, Pet.class);
-//        return pet.setId(id);
-//    }
-//
-//    private int executeUpdate(String statement, Object... params) throws ResponseException {
-//        try (var conn = DatabaseManager.getConnection()) {
-//            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
-//                for (var i = 0; i < params.length; i++) {
-//                    var param = params[i];
-//                    if (param instanceof String p) ps.setString(i + 1, p);
-//                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
-//                    else if (param instanceof PetType p) ps.setString(i + 1, p.toString());
-//                    else if (param == null) ps.setNull(i + 1, NULL);
-//                }
-//                ps.executeUpdate();
-//
-//                var rs = ps.getGeneratedKeys();
-//                if (rs.next()) {
-//                    return rs.getInt(1);
-//                }
-//
-//                return 0;
-//            }
-//        } catch (SQLException e) {
-//            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
-//        }
-//    }
-//
-//    private final String[] createStatements = {
-//            """
-//            CREATE TABLE IF NOT EXISTS  pet (
-//              `id` int NOT NULL AUTO_INCREMENT,
-//              `name` varchar(256) NOT NULL,
-//              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
-//              `json` TEXT DEFAULT NULL,
-//              PRIMARY KEY (`id`),
-//              INDEX(type),
-//              INDEX(name)
-//            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-//            """
-//    };
-//
-//
-//    private void configureDatabase() throws ResponseException {
-//        DatabaseManager.createDatabase();
-//        try (var conn = DatabaseManager.getConnection()) {
-//            for (var statement : createStatements) {
-//                try (var preparedStatement = conn.prepareStatement(statement)) {
-//                    preparedStatement.executeUpdate();
-//                }
-//            }
-//        } catch (SQLException ex) {
-//            throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
-//        }
-//    }
 
     public void addUser(UserData user) {
         confirmDatabaseStructureExists();
@@ -449,7 +333,15 @@ public class SqlDataAccess implements DataAccess {
                     String blackUsername = resultSet.getString("blackUsername");
                     String gameName = resultSet.getString("gameName");
                     String gameJson = resultSet.getString("gameJson");
-                    outGameData = new GameData(retrievedGameID, whiteUsername, blackUsername, gameName, gson.fromJson(gameJson, ChessGame.class));
+
+                    outGameData = new GameData(
+                            retrievedGameID,
+                            whiteUsername,
+                            blackUsername,
+                            gameName,
+                            gson.fromJson(gameJson, ChessGame.class)
+                    );
+
                     return outGameData;
                 } else {
                     throw new ServiceException("Error: Game with ID " + gameID + " not found");
@@ -479,7 +371,14 @@ public class SqlDataAccess implements DataAccess {
                     String blackUsername = resultSet.getString("blackUsername");
                     String gameName = resultSet.getString("gameName");
                     String gameJson = resultSet.getString("gameJson");
-                    GameData outGameData = new GameData(retrievedGameID, whiteUsername, blackUsername, gameName, gson.fromJson(gameJson, ChessGame.class));
+
+                    GameData outGameData = new GameData(
+                            retrievedGameID,
+                            whiteUsername,
+                            blackUsername,
+                            gameName,
+                            gson.fromJson(gameJson, ChessGame.class)
+                    );
 
                     games.put(retrievedGameID, outGameData);
                 }
@@ -515,7 +414,6 @@ public class SqlDataAccess implements DataAccess {
         }
     }
 
-
     public static void addPersonTest(String name, String email, int age) throws DataAccessException, SQLException {
         String insertQuery = "INSERT INTO people (name, email, age) VALUES (?, ?, ?)";
 
@@ -538,22 +436,5 @@ public class SqlDataAccess implements DataAccess {
         } catch (DataAccessException e) {
             throw new RuntimeException(e);
         }
-    }
-
-
-    public void printDatabases() throws SQLException, DataAccessException {
-        DatabaseManager.displayTable(USER_TABLE);
-        DatabaseManager.displayTable(AUTH_TABLE);
-        DatabaseManager.displayTable(GAME_TABLE);
-    }
-
-
-    public static void main(String[] args) throws DataAccessException, SQLException {
-//        DatabaseManager.createTable("people");
-//        addPersonTest("Tyler Chartrand", "tychart@byu.edu", 22);
-        SqlDataAccess sqlDataAccess = new SqlDataAccess();
-        DatabaseManager.displayTable(USER_TABLE);
-        DatabaseManager.displayTable(AUTH_TABLE);
-        DatabaseManager.displayTable(GAME_TABLE);
     }
 }
