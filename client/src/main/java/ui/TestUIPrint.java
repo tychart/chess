f1b20aebc07578ba356aa34029086f9cbcf3f7a8
tychart <tyler@thechartrands.com>
2024-11-13 14:25:53 -0700

    Got the top and the bottom lables on

--- client/src/main/java/ui/TestUIPrint.java
+++ client/src/main/java/ui/TestUIPrint.java
@@ -16,12 +16,9 @@ public class TestUIPrint {
     private static final int LINE_WIDTH_IN_PADDED_CHARS = 1;
 
     // Piece arrays for easy board setup.
-    private static final String[] WHITE_PIECES = {
-            WHITE_ROOK, WHITE_KNIGHT, WHITE_BISHOP, WHITE_QUEEN, WHITE_KING, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK
-    };
-    private static final String[] BLACK_PIECES = {
-            BLACK_ROOK, BLACK_KNIGHT, BLACK_BISHOP, BLACK_QUEEN, BLACK_KING, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK
-    };
+    private static final String[] ROW_LABELS = {"8", "7", "6", "5", "4", "3", "2", "1"};
+    private static final String[] COL_LABELS = {" a ", " b ", " c ", " d ", " e ", " f ", " g ", " h "};
+
 
     ChessGame chessGame;
     ChessBoard chessBoard;
@@ -57,9 +54,13 @@ public class TestUIPrint {
     }
 
     private void drawFlipped(PrintStream out) {
+        drawTopBoarder(out);
+
         for (int row = 0; row < BOARD_SIZE_IN_SQUARES; ++row) {
             drawRowOfSquares(out, row);
         }
+
+        drawBottomBoarder(out);
     }
 
     private void drawRowOfSquares(PrintStream out, int row) {
@@ -86,6 +87,59 @@ public class TestUIPrint {
                     out.print(EMPTY.repeat(SQUARE_SIZE_IN_PADDED_CHARS));
                 }
             }
+            setBlack(out);
+            out.println();
+        }
+    }
+
+    private static void drawTopBoarder(PrintStream out) {
+        drawHorizontalLabels(out);
+        drawHorizontalLine(out);
+    }
+
+    private static void drawBottomBoarder(PrintStream out) {
+        drawHorizontalLine(out);
+        drawHorizontalLabels(out);
+    }
+
+    private static void drawHorizontalLabels(PrintStream out) {
+        int boardLenInPaddedChars = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_PADDED_CHARS;
+
+        setWhite(out);
+        out.print(EMPTY.repeat(boardLenInPaddedChars));
+
+        setBackgroud(out);
+        out.println();
+
+        int prefix = SQUARE_SIZE_IN_PADDED_CHARS / 2;
+        int suffix = SQUARE_SIZE_IN_PADDED_CHARS / 2;
+
+        setWhite(out);
+        for (int i = 0; i < BOARD_SIZE_IN_SQUARES; i++) {
+            out.print(EMPTY.repeat(prefix));
+            out.print(COL_LABELS[i]);
+            out.print(EMPTY.repeat(suffix));
+        }
+
+        setBackgroud(out);
+        out.println();
+
+        setWhite(out);
+        out.print(EMPTY.repeat(boardLenInPaddedChars));
+        setBackgroud(out);
+        out.println();
+
+    }
+
+    private static void drawHorizontalLine(PrintStream out) {
+
+        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_PADDED_CHARS +
+                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_PADDED_CHARS;
+
+        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_PADDED_CHARS; ++lineRow) {
+            setBlack(out);
+            out.print(EMPTY.repeat(boardSizeInSpaces));
+
             setBlack(out);
             out.println();
         }
@@ -133,5 +187,14 @@ public class TestUIPrint {
     private static void setDarkGrey(PrintStream out) {
         out.print(SET_BG_COLOR_DARK_GREY); // Set background to dark gray
         out.print(SET_TEXT_COLOR_BLACK); // Set text to white
+    }
+
+    private static void setRed(PrintStream out) {
+        out.print(SET_BG_COLOR_RED);
+        out.print(SET_TEXT_COLOR_RED);
+    }
+
+    private static void setBackgroud(PrintStream out) {
+        setBlack(out);
     }
 }
